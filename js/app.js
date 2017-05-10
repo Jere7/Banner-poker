c03e5c6eca301bdf0be4d1c45e179046472bceb1
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 14:30:18 -0600

    added return function

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
 });
 
 function startTween(){
-        TweenLite.to($("#toolBox"), 3, {x: 610, y: 300, scale: 1.5, onComplete: returnToNormal });        
+         TweenLite.to($("#toolBox"), 1.5, {x: 610, y: 300, scale: 1.5, onComplete: returnToNormal });        
 
-        TweenLite.to(".color", 3, {color: "#ff0000", fontSize: "+=55"});
+         TweenLite.to(".color", 1.5, { x: 100, y: 330, color: "#ff0000", fontSize: "+=55", onComplete: returnTextToNormal });
 }
 
 function returnToNormal() {
-        TweenLite.to($("#toolBox"), 3, {x: 20, y: 35, scale: 1.2});       
+         TweenLite.to($("#toolBox"), 1.5, {x: 0, y: 0  , scale: 1.2 });       
 }
+
+function returnTextToNormal() {
+         TweenLite.to(".color", 1.5, {x: 100, y: 330, color: "#fff", fontSize: "-=55" });
+}   
