67211b1c9c7fc8695b05c41c40bc1a406d559ca8
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 13:10:06 -0600

    TweenLite.to # and classes

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 });
 
 function startTween(){
-        TweenLite.to($("#toolBox"), 3, {x: 100, y: 200, rotate: 90});        
+        TweenLite.to($("#toolBox"), 3, {x: 350, y: 300, rotate: 360, alpha:.2, scale: 1.5});        
 
         TweenLite.to(".color", 1, {color: "#ff0000"});
 }
