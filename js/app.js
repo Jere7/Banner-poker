bcf988685917d0818324fbd5a9207379936c8e09
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 15:08:55 -0600

    added delay fontSize black

--- js/app.js
+++ js/app.js
@@ -4,18 +4,20 @@ $(document).ready(function(){
 });
 
 function startTween(){
-         TweenLite.to($("#toolBox"), 1.5, {x: 610, y: 300, scale: 1.5, onComplete: returnToNormal, onCompleteParams: [$("#toolBox")]});        
+         TweenLite.to($("#toolBox"), 1.5, {delay: 1, x: 610, y: 300, scale: 1.5,
+              onComplete: returnToNormal, onCompleteParams: [$("#toolBox")]});        
 
-         TweenLite.to(".color", 1.5, { x: 100, y: 330, color: "#ff0000", fontSize: "+=55", onComplete: returnTextToNormal });
+         TweenLite.to(".color", 1.5, { x: 700, y: 330,
+              color: "#ff0000", fontSize: "+=55", onComplete: returnTextToNormal });
 }
 
 function returnToNormal(obj) {
     if(obj){
-         TweenLite.to(obj, 1.5, {x: 0, y: 0  , scale: 1.2 });       
+         TweenLite.to(obj, 1.5, {x: 100, y: 150  , scale: 1.2 });       
     }
-        
+            
 }
 
 function returnTextToNormal() {
-         TweenLite.to(".color", 1.5, {x: 100, y: 330, color: "#fff", fontSize: "-=55" });
+         TweenLite.to(".color", 1.5, {x: 100, y: 330, color: "black", fontSize: "-=40" });
 }   
