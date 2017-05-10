001aeeb6a17e955d980aeb5821f4bbfd542063b5
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 14:51:10 -0600

    use the conditional params if obj

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@ $(document).ready(function(){
 });
 
 function startTween(){
-         TweenLite.to($("#toolBox"), 1.5, {x: 610, y: 300, scale: 1.5, onComplete: returnToNormal });        
+         TweenLite.to($("#toolBox"), 1.5, {x: 610, y: 300, scale: 1.5, onComplete: returnToNormal, onCompleteParams: [$("#toolBox")]});        
 
          TweenLite.to(".color", 1.5, { x: 100, y: 330, color: "#ff0000", fontSize: "+=55", onComplete: returnTextToNormal });
 }
 
-function returnToNormal() {
-         TweenLite.to($("#toolBox"), 1.5, {x: 0, y: 0  , scale: 1.2 });       
+function returnToNormal(obj) {
+    if(obj){
+         TweenLite.to(obj, 1.5, {x: 0, y: 0  , scale: 1.2 });       
+    }
+        
 }
 
 function returnTextToNormal() {
