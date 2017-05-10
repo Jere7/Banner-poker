54528b627221a3711ae85d06bd86193f5f884944
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 13:50:06 -0600

    change a fontSize css selectorcolor to 150 ++

--- js/app.js
+++ js/app.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
 function startTween(){
         TweenLite.to($("#toolBox"), 3, {x: 600, y: 300, rotation: 360, alpha: .2});        
 
-        TweenLite.to(".color", 3, {color: "#ff0000"});
+        TweenLite.to(".color", 3, {color: "#ff0000", fontSize: "150"});
 }
 
