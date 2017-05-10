e607354b30fc7ea04c2ed2f3f43e9958f3f05ef0
Jeremias Gatjens <7gatjen@gmail.com>
2017-05-10 09:13:22 -0600

    tweenlite.to

--- js/app.js
+++ js/app.js
@@ -7,4 +7,4 @@
 // document.addEventListener("DOMContentLoaded", function(){
 //         alert("Pure JS Ready");
 // });
-
+TweenLite.to("#rect", 1, {attr:{x:100, y:50, width:100, height:100}, ease:Linear.easeNone});
